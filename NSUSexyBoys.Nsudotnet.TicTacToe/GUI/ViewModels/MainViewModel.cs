22a93aab5285577622f68e9dfbc1fef34ec48b5c
Ivan Shmakov <i.s.shmakov@gmail.com>
2015-05-11 17:01:39 +0600

    MainViewModel started

--- NSUSexyBoys.Nsudotnet.TicTacToe/GUI/ViewModels/MainViewModel.cs
+++ NSUSexyBoys.Nsudotnet.TicTacToe/GUI/ViewModels/MainViewModel.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Caliburn.Micro;
+using Logic;
 
 namespace GUI.ViewModels
 {
-    class MainViewModel : Screen
+    class MainViewModel : PropertyChangedBase
     {
+        private ITicTacToeService _game;
+
+        public ObservableCollection<FieldElement> _mainFieldElements {get; set; }
+        public ObservableCollection<FieldElement> _subFieldElements  {get; set; }
+
+        public string _firstPlayerName  { get; set; }
+        public string _secondPlayerName { get; set; }
+
+        public int _currentPlayer { get; set; }
+
+
+
+
     }
 }
