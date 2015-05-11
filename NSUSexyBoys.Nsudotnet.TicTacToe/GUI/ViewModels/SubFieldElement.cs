5c85687d822733a0f6ead60344e1dae6202e2fe5
Stanislav Pospelov <stpospelov@gmail.com>
2015-05-11 16:30:07 +0600

    Fixed3

--- NSUSexyBoys.Nsudotnet.TicTacToe/GUI/ViewModels/SubFieldElement.cs
+++ NSUSexyBoys.Nsudotnet.TicTacToe/GUI/ViewModels/SubFieldElement.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Data;
 
 namespace GUI.ViewModels
 {
     class SubFieldElement: FieldElement
     {
-        public SubFieldElement(int row, int column, int value) : base(row, column, value)
+        public SubFieldElement(int row, int column, Condition value) : base(row, column, value)
         {
             SizeText = 10;
         }
